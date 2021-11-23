5542a954d5bd7e2175c61d9542aa43474da77abe
Jacob Asuncion <asuncion1173@gmail.com>
2021-11-23 21:36:12 +0800

    Added more labels for questions, checklist, and radiobuttons for choices

--- ContactTracing/Form1.Designer.cs
+++ ContactTracing/Form1.Designer.cs
@@ -29,13 +29,326 @@ namespace ContactTracing
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxFN = new System.Windows.Forms.TextBox();
+            this.fullName = new System.Windows.Forms.Label();
+            this.emailAdd = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.residence = new System.Windows.Forms.Label();
+            this.textBoxResidence = new System.Windows.Forms.TextBox();
+            this.contactNumber = new System.Windows.Forms.Label();
+            this.textBoxContact = new System.Windows.Forms.TextBox();
+            this.gender = new System.Windows.Forms.Label();
+            this.textBoxGender = new System.Windows.Forms.TextBox();
+            this.healthCheckList = new System.Windows.Forms.CheckedListBox();
+            this.noneCheckBox = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateVisit = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxFN
+            // 
+            this.textBoxFN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFN.Location = new System.Drawing.Point(12, 87);
+            this.textBoxFN.Name = "textBoxFN";
+            this.textBoxFN.Size = new System.Drawing.Size(264, 23);
+            this.textBoxFN.TabIndex = 0;
+            // 
+            // fullName
+            // 
+            this.fullName.AutoSize = true;
+            this.fullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullName.Location = new System.Drawing.Point(12, 68);
+            this.fullName.Name = "fullName";
+            this.fullName.Size = new System.Drawing.Size(79, 17);
+            this.fullName.TabIndex = 1;
+            this.fullName.Text = "Full Name: ";
+            // 
+            // emailAdd
+            // 
+            this.emailAdd.AutoSize = true;
+            this.emailAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailAdd.Location = new System.Drawing.Point(322, 171);
+            this.emailAdd.Name = "emailAdd";
+            this.emailAdd.Size = new System.Drawing.Size(102, 17);
+            this.emailAdd.TabIndex = 3;
+            this.emailAdd.Text = "Email Address:";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.Location = new System.Drawing.Point(322, 190);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(287, 23);
+            this.textBoxEmail.TabIndex = 2;
+            // 
+            // residence
+            // 
+            this.residence.AutoSize = true;
+            this.residence.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.residence.Location = new System.Drawing.Point(12, 117);
+            this.residence.Name = "residence";
+            this.residence.Size = new System.Drawing.Size(83, 17);
+            this.residence.TabIndex = 5;
+            this.residence.Text = "Residence: ";
+            // 
+            // textBoxResidence
+            // 
+            this.textBoxResidence.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxResidence.Location = new System.Drawing.Point(12, 136);
+            this.textBoxResidence.Name = "textBoxResidence";
+            this.textBoxResidence.Size = new System.Drawing.Size(264, 23);
+            this.textBoxResidence.TabIndex = 4;
+            // 
+            // contactNumber
+            // 
+            this.contactNumber.AutoSize = true;
+            this.contactNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactNumber.Location = new System.Drawing.Point(12, 171);
+            this.contactNumber.Name = "contactNumber";
+            this.contactNumber.Size = new System.Drawing.Size(118, 17);
+            this.contactNumber.TabIndex = 7;
+            this.contactNumber.Text = "Contact Number: ";
+            // 
+            // textBoxContact
+            // 
+            this.textBoxContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxContact.Location = new System.Drawing.Point(12, 190);
+            this.textBoxContact.Name = "textBoxContact";
+            this.textBoxContact.Size = new System.Drawing.Size(237, 23);
+            this.textBoxContact.TabIndex = 6;
+            // 
+            // gender
+            // 
+            this.gender.AutoSize = true;
+            this.gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gender.Location = new System.Drawing.Point(322, 116);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(60, 17);
+            this.gender.TabIndex = 9;
+            this.gender.Text = "Gender:";
+            // 
+            // textBoxGender
+            // 
+            this.textBoxGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxGender.Location = new System.Drawing.Point(322, 136);
+            this.textBoxGender.Name = "textBoxGender";
+            this.textBoxGender.Size = new System.Drawing.Size(121, 23);
+            this.textBoxGender.TabIndex = 8;
+            // 
+            // healthCheckList
+            // 
+            this.healthCheckList.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.healthCheckList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.healthCheckList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthCheckList.FormattingEnabled = true;
+            this.healthCheckList.HorizontalScrollbar = true;
+            this.healthCheckList.Items.AddRange(new object[] {
+            "Fever",
+            "Dry Cough",
+            "Sore Throat",
+            "Shortness of Breath",
+            "Loss of Smell and Taste",
+            "Fatigue",
+            "Body Pain",
+            "Runny Nose",
+            "Diarhea",
+            "Headache"});
+            this.healthCheckList.Location = new System.Drawing.Point(322, 268);
+            this.healthCheckList.Name = "healthCheckList";
+            this.healthCheckList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.healthCheckList.Size = new System.Drawing.Size(286, 198);
+            this.healthCheckList.TabIndex = 10;
+            // 
+            // noneCheckBox
+            // 
+            this.noneCheckBox.AutoSize = true;
+            this.noneCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noneCheckBox.Location = new System.Drawing.Point(322, 472);
+            this.noneCheckBox.Name = "noneCheckBox";
+            this.noneCheckBox.Size = new System.Drawing.Size(145, 21);
+            this.noneCheckBox.TabIndex = 11;
+            this.noneCheckBox.Text = "None of the Above";
+            this.noneCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(322, 87);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(299, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(127, 590);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(85, 30);
+            this.btnSubmit.TabIndex = 13;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.AutoSize = true;
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeader.Location = new System.Drawing.Point(206, 9);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(107, 26);
+            this.labelHeader.TabIndex = 14;
+            this.labelHeader.Text = "Safe Stay";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 22);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Contact Tracing Form";
+            // 
+            // dateVisit
+            // 
+            this.dateVisit.AutoSize = true;
+            this.dateVisit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateVisit.Location = new System.Drawing.Point(322, 67);
+            this.dateVisit.Name = "dateVisit";
+            this.dateVisit.Size = new System.Drawing.Size(84, 17);
+            this.dateVisit.TabIndex = 16;
+            this.dateVisit.Text = "Date of Visit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 34);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Are you experiencing or did you have \r\nany of the following in the last 14 days?\r" +
+    "\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 504);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 68);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Have you had face-to-face contact \r\nwith a probable or confirmed COVID-19 \r\ncase " +
+    "within 1 meter and for more than \r\n15 minutes for the past 14 days?";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(325, 519);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 19;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(416, 519);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 20;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.listBox1.Location = new System.Drawing.Point(60, 371);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(670, 621);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateVisit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelHeader);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.noneCheckBox);
+            this.Controls.Add(this.healthCheckList);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.textBoxGender);
+            this.Controls.Add(this.contactNumber);
+            this.Controls.Add(this.textBoxContact);
+            this.Controls.Add(this.residence);
+            this.Controls.Add(this.textBoxResidence);
+            this.Controls.Add(this.emailAdd);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.fullName);
+            this.Controls.Add(this.textBoxFN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxFN;
+        private System.Windows.Forms.Label fullName;
+        private System.Windows.Forms.Label emailAdd;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label residence;
+        private System.Windows.Forms.TextBox textBoxResidence;
+        private System.Windows.Forms.Label contactNumber;
+        private System.Windows.Forms.TextBox textBoxContact;
+        private System.Windows.Forms.Label gender;
+        private System.Windows.Forms.TextBox textBoxGender;
+        private System.Windows.Forms.CheckedListBox healthCheckList;
+        private System.Windows.Forms.CheckBox noneCheckBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label dateVisit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
